634bf0177f8069c497407cbb8b6f3015baa966d0
jsolarz <jsolarz@google.com>
2022-02-28 13:30:17 +0100

    Add event handling to server.py

--- server.py
+++ server.py
@@ -51,18 +51,30 @@ def stripe_webhook():
         print('invalid payload')
         return "Invalid payload", 400
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        print('Invalid signature')
-        return "Invalid signature", 400
+        print('⚠️  Webhook signature verification failed.' + str(e))
+        return jsonify(success=False)
 
+    # Handle the event
+    if event and event['type'] == 'payment_intent.succeeded':
+        payment_intent = event['data']['object']  # contains a stripe.PaymentIntent
+        print('Payment for {} succeeded'.format(payment_intent['amount']))
+        # Then define and call a method to handle the successful payment intent.
+        # handle_payment_intent_succeeded(payment_intent)
+    elif event['type'] == 'payment_method.attached':
+        payment_method = event['data']['object']  # contains a stripe.PaymentMethod
+        # Then define and call a method to handle the successful attachment of a PaymentMethod.
+        # handle_payment_method_attached(payment_method)
     # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
+    elif event['type'] == 'checkout.session.completed':
         session = event['data']['object']
 
         # Fulfill the purchase...
         fulfill_order(session)
+    else:
+        # Unexpected event type
+        print('Unhandled event type {}'.format(event['type']))
 
-    return event['type']
+    return jsonify(success=True)
 
 def fulfill_order(session):
   # TODO: fill me in
